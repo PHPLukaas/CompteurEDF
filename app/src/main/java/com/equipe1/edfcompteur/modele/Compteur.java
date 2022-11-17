9dc37f313938ece55f9d234c69986ed349d20ae8
Lukas <lukasvalois@gmail.com>
2022-11-17 14:05:39 +0100

    rattrapage

---
+++ app/src/main/java/com/equipe1/edfcompteur/modele/Compteur.java
@@ -0,0 +1,47 @@
+package com.equipe1.edfcompteur.modele;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "compteur",
+        foreignKeys = {@ForeignKey(entity = Client.class,
+                parentColumns = "idClient",
+                childColumns = "idClientCompteur",
+                onDelete = ForeignKey.CASCADE)
+        })
+public class Compteur {
+    @PrimaryKey (autoGenerate = true)
+    @ColumnInfo (name = "idCompteur")
+    @NonNull
+    private int idCompteur;
+
+    @ColumnInfo (name = "idClientCompteur")
+    private long idClientCompteur;
+
+    public Compteur() {
+    }
+
+    public Compteur(int idCompteur, long idClientCompteur) {
+        this.idCompteur = idCompteur;
+        this.idClientCompteur = idClientCompteur;
+    }
+
+    public int getIdCompteur() {
+        return idCompteur;
+    }
+
+    public void setIdCompteur(int idCompteur) {
+        this.idCompteur = idCompteur;
+    }
+
+    public long getIdClientCompteur() {
+        return idClientCompteur;
+    }
+
+    public void setIdClientCompteur(long idClientCompteur) {
+        this.idClientCompteur = idClientCompteur;
+    }
+}
