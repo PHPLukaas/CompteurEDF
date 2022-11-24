52744d069451e78b13f734dd2c7f17179e2d7ff2
Lukas <lukasvalois@gmail.com>
2022-11-24 16:36:06 +0100

    test

--- app/src/main/java/com/equipe1/edfcompteur/controller/ClientsListActivity.java
+++ app/src/main/java/com/equipe1/edfcompteur/controller/ClientsListActivity.java
@@ -3,6 +3,7 @@ package com.equipe1.edfcompteur.controller;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.equipe1.edfcompteur.R;
+import com.equipe1.edfcompteur.view.client.ClientViewHolder;
 
 public class ClientsListActivity extends AppCompatActivity {
 
@@ -11,5 +12,6 @@ public class ClientsListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clients_list);
 
+        ClientViewHolder clientViewHolder = new ClientViewHolder(this.getCurrentFocus());
     }
 }
