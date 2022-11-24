df4d60393bd3c05eea71ed4632d2dfa7f5ba34a5
Lukas <lukasvalois@gmail.com>
2022-11-24 15:06:11 +0100

    DAO + ViewModel

--- app/src/main/java/com/equipe1/edfcompteur/MainActivity.java
+++ app/src/main/java/com/equipe1/edfcompteur/MainActivity.java
@@ -1,5 +1,7 @@
 package com.equipe1.edfcompteur;
 
+import android.content.Intent;
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View mImageViewIdentification = findViewById(R.id.TxtClients);
+        mImageViewIdentification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Identification = new Intent(MainActivity.this, activity_client.class);
+                startActivity(Identification);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/equipe1/edfcompteur/view/ClientViewModel.java
+++ app/src/main/java/com/equipe1/edfcompteur/view/ClientViewModel.java
@@ -9,12 +9,12 @@ import com.equipe1.edfcompteur.modele.Client;
 
 import java.util.List;
 
-public class ViewModel extends AndroidViewModel {
+public class ClientViewModel extends AndroidViewModel {
 
     private ClientRepository mClient;
     private final LiveData<List<Client>> mAllClients;
 
-    public ViewModel(@NonNull Application application) {
+    public ClientViewModel(@NonNull Application application) {
         super(application);
         this.mClient = new ClientRepository(application);
         this.mAllClients = mClient.getAllClients();
